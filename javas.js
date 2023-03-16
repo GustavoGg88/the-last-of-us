1dbc4d57b513b9dbfb8a9efb0e705b4722be6cef
GustavoGg88 <71441645+GustavoGg88@users.noreply.github.com>
2023-03-16 10:22:38 -0300

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,36 @@
+// passo 1
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+    // passo 2 - dar um jeito de identificar o clique do usuário no botão
+	botao.addEventListener('click', () => {
+		
+		// passo 3 - desmarcar o botão selecionado anterior
+		desativarBotaoSelecionado();
+		
+		// passo 4 - marcar o botão clicado como se estivesse selecionado
+		botao.classList.add('selecionado');
+
+        // passo 5 - esconder a imagem ativa anterior
+		esconderImagemAtiva();
+
+        // passo 6 - fazer aparecer a imagem correspondente ao botão clicado
+        mostrarImagemDeFundo(indice);
+    });
+});
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+    botoesCarrossel[indice].classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
